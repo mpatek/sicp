f649212fcda822d20c57cf6b08de8ff8cbd5903d
Mike Patek <mpatek@gmail.com>
2018-09-16 17:47:16 -0400

    Add ex-1-24.lisp

---
+++ chapter-1/ex-1-24.lisp
@@ -0,0 +1,42 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	  (remainder (* base (expmod base (- exp 1) m))
+		     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+(define (divides? a b )
+  (= (remainder b a) 0))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes a b)
+  (cond ((divides? 2 a) (search-for-primes (+ a 1) b))
+	((> a b))
+	(else
+	  (timed-prime-test a)
+	  (search-for-primes (+ a 2) b))))
+
