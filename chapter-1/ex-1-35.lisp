b73764f18e5b5e40260a54229eb78df6758b259e
Mike Patek <mpatek@gmail.com>
2018-12-30 10:15:13 -0500

    Added Ex 1-35

---
+++ chapter-1/ex-1-35.lisp
@@ -0,0 +1,14 @@
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? next guess)
+	next
+	(try next))))
+  (try first-guess))
+
+(define golden-ratio
+  (fixed-point (lambda (x) (+ 1 (/ 1.0 x))) 1.0))
