4457a3b68e17d488bd3028cd2b0c0f38bb10104d
Mike Patek <mpatek@gmail.com>
2019-01-06 15:41:07 -0500

    Add Ex 1-45

---
+++ chapter-1/ex-1-45.lisp
@@ -0,0 +1,50 @@
+(define (average x y) (/ (+ x y) 2))
+
+(define tolerance 0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess tries)
+    (if (= tries 0)
+      (error "i give up!")
+      (let ()
+	(display "guess: ")
+	(display guess)
+	(newline)
+	(let ((next (f guess)))
+	  (if (close-enough? next guess)
+	    next
+	    (try next (- tries 1)))))))
+  (try first-guess 10000))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define dx 0.00001)
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+	       1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (* y y))))
+	       1.0))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1)
+    f
+    (repeated (compose f f) (- n 1))))
+
+(define (nth-root x n)
+  (define (find-avg-damp-repetitions n)
+    (define (test e r s)
+      (if (< n (expt 2 e))
+        r
+        (test (+ e s) (+ r 1) (* s 2))))
+    (test 2 1 1))
+  (fixed-point ((repeated average-damp (find-avg-damp-repetitions n)) (lambda (y) (/ x (expt y (- n 1)))))
+	       1.0))
