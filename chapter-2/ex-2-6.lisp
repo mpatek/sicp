3083143cd6f28b1fdb5174dbd8e6d296c91e2a04
Mike Patek <mpatek@gmail.com>
2019-01-20 10:06:16 -0500

    Add Ex 2-6

---
+++ chapter-2/ex-2-6.lisp
@@ -0,0 +1,18 @@
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one
+  (lambda (f) (lambda (x) (f ((
+			       (lambda (f) (lambda (x) x))
+			       f) x)))))
+
+(define two
+  (lambda (f) (lambda (x) (f ((
+			       (lambda (f) (lambda (x) (f ((
+							    (lambda (f) (lambda (x) x))
+							    f) x)))))
+			      f) x))))
+
+(define (+ f g) (f g))
