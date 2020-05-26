4cb65d76902cb38de5fafc87875b31b1ce5a43e0
Mike Patek <mpatek@gmail.com>
2020-05-25 21:07:16 -0400

    Add ex 3-8

---
+++ chapter-3/ex-3-8.lisp
@@ -0,0 +1,14 @@
+(define (get-f)
+  (define f
+    (let ((counter 0))
+      (lambda (x)
+	(let ((answer (/ (abs (- x counter)) 2)))
+	  (set! counter (+ counter 1))
+	  answer))))
+  f)
+
+(define f1 (get-f))
+(+ (f1 0) (f1 1))
+
+(define f2 (get-f))
+(+ (f2 1) (f2 0))
